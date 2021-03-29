286720229c45b28da6d7e3fa3233205c2eb8b47e
Nick Friday <nfriday@yandex.ru>
2021-03-29 23:12:40 +0300

    rm: man page makes no sence

--- config.mk
+++ config.mk
@@ -5,14 +5,13 @@ VERSION = nf
 
 # paths
 PREFIX = /usr/local
-MANPREFIX = ${PREFIX}/share/man
 
 X11INC = /usr/X11R6/include
 X11LIB = /usr/X11R6/lib
 
 # Xinerama, comment if you don't want it
-XINERAMALIBS  = -lXinerama
-XINERAMAFLAGS = -DXINERAMA
+# XINERAMALIBS  = -lXinerama
+# XINERAMAFLAGS = -DXINERAMA
 
 # freetype
 FREETYPELIBS = -lfontconfig -lXft
